7ade9b3a7f2bec9770632992fb984ccb5dfb813b
gdmec07150712 <1613478804@qq.com>
2016-12-27 14:24:36 +0800

    Initial commit

---
+++ app/src/main/java/s07150712/gdmec/edu/cn/myweather/MainActivity.java
@@ -0,0 +1,13 @@
+package s07150712.gdmec.edu.cn.myweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
